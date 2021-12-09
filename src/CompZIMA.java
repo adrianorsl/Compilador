527bb6343a21e9fcab2fef991b473bef6d0b8cbb
Adriano Zimmermann <adrianorslsc@hotmail.com>
2021-12-09 17:14:22 -0300

    Initial commit

---
+++ src/CompZIMA.java
@@ -0,0 +1,21 @@
+
+public class CompZIMA {
+	
+	public static void main(String args[]){
+		try{
+			MeuLexer lexer = new MeuLexer(System.in);
+			MeuParser parser = new MeuParser(lexer);
+
+			System.out.println("Starting Compiling Process...");
+
+			parser.prog();
+			
+			System.out.println("Compilation Finishted!");
+
+		}
+		catch(Exception ex){
+			System.err.println(ex.getMessage());
+		}
+	}
+
+}
